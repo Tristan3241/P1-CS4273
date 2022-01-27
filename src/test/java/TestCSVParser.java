e42b2e9d95447221a9beb07b22544cd35273ec2f
Chance Hughes <55999007+Chancegh@users.noreply.github.com>
2022-01-27 13:23:12 -0600

    Updated 2 j unit tests

--- src/test/java/TestCSVParser.java
+++ src/test/java/TestCSVParser.java
@@ -3,9 +3,28 @@ import org.junit.jupiter.api.Test;
 
 
 public class TestCSVParser {
-    public TestCSVParser() {
-      
-    }
-  @Test 
-  
+  // Tests if the length of the parsed array is equal to what is expected
+	@Test
+	public void lengthTest() throws FileNotFoundException {
+		CSVParser myCSVParser = new CSVParser();
+		File csv = new File("alphabets.csv");
+		ArrayList<String> actual = myCSVParser.readCsv(csv);
+		ArrayList<String> expectedLength = new ArrayList<String>();
+		
+		for(int i=0;i<55;i++) {
+			int temp = i;
+			String s = String.valueOf(temp);
+			expectedLength.add(s);
+		}
+		assertEquals(expectedLength.size(), actual.size());
+	}
+	
+	// Tests if the parsed array is empty or not
+	@Test
+	public void emptyTest() throws FileNotFoundException {
+		CSVParser myCSVParser = new CSVParser();
+		File csv = new File("alphabets.csv");
+		ArrayList<String> actual = myCSVParser.readCsv(csv);
+		assertFalse(actual.isEmpty());
+	}
 }
