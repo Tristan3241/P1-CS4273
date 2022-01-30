7324a041483b0c13867f2916a9641eef7a8ac5a7
Tristan3241 <47228117+Tristan3241@users.noreply.github.com>
2022-01-30 13:07:51 -0600

    Update TestCSVParser.java

--- src/test/java/TestCSVParser.java
+++ src/test/java/TestCSVParser.java
@@ -32,12 +32,12 @@ public class TestCSVParser {
 	}
 	//Tests if the CSV parser correctly parses the english langage
 	@Test
-	public void parseTestEnglish() throws FileNotFoundException {
+	public void parseTest() throws FileNotFoundException {
 		CSVParser myCSVParser = new CSVParser();
 		File csv = new File("alphabets.csv");
 		ArrayList<String> actual = myCSVParser.readCsv(csv);
 		ArrayList<String> expected = new ArrayList<String>(
-			Arrays.asList("English", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"));
+			Arrays.asList("English", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Spanish", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"));
 		assertArrayEquals(expected.toArray(), actual.toArray());
 		
 		
