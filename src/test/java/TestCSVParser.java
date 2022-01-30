fbe97f6a639fb8c968dba7339263816ddf774ded
Tristan3241 <47228117+Tristan3241@users.noreply.github.com>
2022-01-30 12:51:38 -0600

    Update TestCSVParser.java

--- src/test/java/TestCSVParser.java
+++ src/test/java/TestCSVParser.java
@@ -36,7 +36,7 @@ public class TestCSVParser {
 		File csv = new File("alphabets.csv");
 		ArrayList<String> actual = myCSVParser.readCsv(csv);
 		ArrayList<String> expected = new ArrayList<String>(
-			Arrays.asList(English, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z);
+			Arrays.asList(English, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z));
 		assertArrayEquals(expected, actual);
 		
 		
